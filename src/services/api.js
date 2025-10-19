c14adafacd44c214db6d86e6102b9cb93bca1f56
kuro-shiv <dubeysk2003@gmail.com>
2025-10-19 20:53:28 +0530

    api change

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-// Hardcoded backend URL
 const API_URL = "https://sno-relax-server.onrender.com/api/admin";
-const COMMUNITY_URL = "https://sno-relax-server.onrender.com/api/community";
+const COMMUNITY_URL = `${API_URL}/community`;
 
 // ------------------ USERS ------------------
 export const getUsers = () => axios.get(`${API_URL}/users`);
@@ -11,10 +10,11 @@ export const createUser = (userData) => axios.post(`${API_URL}/users`, userData)
 export const updateUser = (id, updatedData) => axios.put(`${API_URL}/users/${id}`, updatedData);
 export const deleteUser = (id) => axios.delete(`${API_URL}/users/${id}`);
 
-// Admin login
+// ------------------ ADMIN LOGIN ------------------
 export const loginAdmin = (credentials) => axios.post(`${API_URL}/login`, credentials);
 
-// Chat stats
+// ------------------ STATS ------------------
+export const getStats = () => axios.get(`${API_URL}/stats`);
 export const getChatStats = () => axios.get(`${API_URL}/stats/chats`);
 
 // ------------------ CONTENT ------------------
@@ -24,10 +24,7 @@ export const createContent = (data) => axios.post(`${API_URL}/content`, data);
 export const updateContent = (id, data) => axios.put(`${API_URL}/content/${id}`, data);
 export const deleteContent = (id) => axios.delete(`${API_URL}/content/${id}`);
 
-// ------------------ STATS ------------------
-export const getStats = () => axios.get(`${API_URL}/stats`);
-
 // ------------------ COMMUNITY ------------------
-export const fetchGroups = () => axios.get(`${COMMUNITY_URL}/groups`).then(res => res.data);
-export const createGroup = (data) => axios.post(`${COMMUNITY_URL}/create`, data).then(res => res.data);
+export const fetchGroups = () => axios.get(COMMUNITY_URL).then(res => res.data);
+export const createGroup = (data) => axios.post(COMMUNITY_URL, data).then(res => res.data);
 export const deleteGroup = (groupId) => axios.delete(`${COMMUNITY_URL}/${groupId}`).then(res => res.data);
